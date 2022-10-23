4500371c29c253b9608dca3132e99532bc86da3a
김도언 <vov06144@naver.com>
2022-10-23 16:03:07 +0900

    Upload insta_clone_coding (incomplete)

---
+++ Django_study/instagram_clone_coding/accounts/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, redirect
+
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+
+from .forms import CustomUserCreationForm
+
+# Create your views here.
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('posts:index')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('posts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+def logout(request):
+    if request.user.is_authenticated:
+        auth_logout(request)
+    return redirect('accounts:login')
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('posts:index')
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('posts:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+
+def delete(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            request.user.delete()
+            auth_logout(request)
+            return redirect('posts:index')
+        else:
+            return render(request, 'accounts/delete.html')
+    return redirect('accounts:login')
+
+
+def update(request):
+    if request.method == 'POST':
+        pass
+    else:
+        
---
+++ Django_study/instagram_clone_coding/posts/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
+
+# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+    else:
+        form = PostForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'posts/create.html', context)
+    
+
+def delete(request, pk):
+    post = Post.objects.get(pk=pk)
+    post.delete()
+    return redirect('posts:index')
+
+
+def update(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+    else:
+        form = PostForm(instance=post)
+    context = {
+        'form': form,
+        'post': post,
+    }
+    return render(request, 'posts/update.html', context)
