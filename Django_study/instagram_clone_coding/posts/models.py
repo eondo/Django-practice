2e877af345308f00d47a97ad87e9ec7a5e9fa5a3
eondo <vov06144@naver.com>
2022-10-27 17:55:38 +0900

    Add thumbnail image sizing

--- Django_study/instagram_clone_coding/posts/models.py
+++ Django_study/instagram_clone_coding/posts/models.py
@@ -1,6 +1,15 @@
+from multiprocessing.dummy import Process
 from django.db import models
+from imagekit.processors import ResizeToFit, Thumbnail
+from imagekit.models import ProcessedImageField
 
 # Create your models here.
 class Post(models.Model):
     content = models.TextField()
-    image = models.ImageField(blank=True)
+    image = ProcessedImageField(
+        blank=True,
+        upload_to='thumbnails/',
+        processors=[Thumbnail(200, 300)],
+        format='JPEG',
+        options={'quality': 80},
+    )
